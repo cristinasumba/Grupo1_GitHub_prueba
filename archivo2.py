f1c74760c5b3d36644c78e7db3cb2b159f799818
cristinasumba <veritocristina@hotmail.com>
2021-08-01 18:25:39 -0500

    Creacion objetos

--- archivo2.py
+++ archivo2.py
@@ -11,3 +11,10 @@ class Empleado(Persona):
 
     def setSalario(self, salarion):
         self.salario=salarion
+
+#creacion de los objetos
+oPersona=Persona("Juan", 19)
+oEmpleado=Empleado("Mery", 25, 500)
+
+print(oPersona.getEdad())
+print(oEmpleado.getSalario())
