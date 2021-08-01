41f42de78089996aa468e7773285be972851886b
cristinasumba <veritocristina@hotmail.com>
2021-08-01 18:22:50 -0500

    Metodos get y set (Empleado)

--- archivo2.py
+++ archivo2.py
@@ -4,3 +4,10 @@ class Empleado(Persona):
     def __init__(self, nombre, edad, salario):
         super().__init__(nombre, edad)
         self.salario=salario
+
+    #creacion de los metodos get y set
+    def getSalario(self):
+        return self.salario
+
+    def setSalario(self, salarion):
+        self.salario=salarion
