1a83bcff7672e830e6fadd64e1b8ad01c0a43038
cristinasumba <veritocristina@hotmail.com>
2021-08-01 18:15:02 -0500

    Creacion de los metodos get y set (Persona)

--- archivo1.py
+++ archivo1.py
@@ -2,3 +2,14 @@ class Persona ():
     def __init__(self, nombre,edad):
         self.nombre=nombre
         self.edad=edad
+        
+        #creacion de los metodos get y set
+    def getNombre(self):
+        return self.nombre
+
+    def getEdad(self):
+        return self.edad
+
+    def setNombre(self, nombren):
+        self.nombre=nombren
+
