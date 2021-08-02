ac014d85ca9e1192e6076568a4c3bea7108b9e7d
cristinasumba <veritocristina@hotmail.com>
2021-08-01 20:20:50 -0500

    Creacion clase hija (Estudiante), metdos set y get, +oEstudiante

--- archivo2.py
+++ archivo2.py
@@ -12,9 +12,31 @@ class Empleado(Persona):
     def setSalario(self, salarion):
         self.salario=salarion
 
+#creacion de una clase hija (Estudiante)
+class Estudiante(Persona):
+    def __init__(self, nombre, edad, correo=None, telefono=None):
+        super().__init__(nombre, edad)
+        self.correo=str(input("Ingrese su correro electrónico: "))
+        self.telefono=str(input("Ingrese su número de teléfono: "))
+
+    #Metodos set y get
+    def getCodigo(self):
+        return self.correo
+
+    def getTelefono(self):
+        return self.telefono
+
+    def setCodigo(self, correon):
+        self.correo=correon
+
+    def setTelefono(self, telefonon):
+        self.telefono=telefonon
+        
+
 #creacion de los objetos
 oPersona=Persona("Juan", 19)
 oEmpleado=Empleado("Mery", 25, 500)
+oEstudiante=Estudiante("Marco", 14)
 
 print(oPersona.getEdad())
 print(oEmpleado.getSalario())
