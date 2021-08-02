969d85e1f2817249217ae054b631ab4fc4006cf7
AlbertoCalle22 <albertocalle_2002@hotmail.com>
2021-08-01 19:16:04 -0500

    Creacion clase hija (Profesor)

--- archivo1.py
+++ archivo1.py
@@ -21,4 +21,22 @@ oPersona2.setNombre("Shirley")
 
 print (oPersona2.getNombre())
 
+#creacion de la clase hija profesor
+class Profesor(Persona):
+    def __init__(self, nombre, edad, titulo, aexp):
+        super().__init__(nombre, edad)
+        self.titulo=titulo
+        self.aexp=aexp
+
+    def getTitulo(self):
+        return self.titulo
+
+    def getAexp(self):
+        return self.aexp
+
+    def setTitulo(self, titulon):
+        self.titulo=titulon
+
+    def setAexp(self, aexpn):
+        self.aexp=aexpn
 
